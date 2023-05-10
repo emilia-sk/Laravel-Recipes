64d8c473426cbc783d83603dd9fefcd0a1244d5e
Emilia <emilia66x@gmail.com>
2023-05-10 16:39:58 +0300

    Update files

--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -1,9 +1,10 @@
 <div class="col-12 d-flex justify-content-center mx-4 pt-2 pt-lg-0 pb-0 pb-lg-4"><!--SEARCH BAR-->
     <div class="col-12 col-lg-3">
         <form action="/recipes-all" class="input-group">
-            <input type="text" class="form-control" name="search" placeholder="Search recipes...">
+            <input type="text" class="form-control" id="searchBar" name="search"
+                placeholder="Search recipes...">
             <button type="submit" class="btn btn-search mb-2">
-                <img class="icon-search" src="{{asset('images/icon-search.png')}}" </button>
+                <img class="icon-search" src="{{asset('images/icon-search.png')}}"></button>
         </form>
     </div>
 </div>
