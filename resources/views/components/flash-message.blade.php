0e131cea2b6af27eb859696821efe8d255701169
Emilia <emilia66x@gmail.com>
2023-05-12 18:01:00 +0300

    update files

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,3 +1,4 @@
 @if(session()->has('message'))
-<p class="flash-message">{{session('message')}}</p>
+<p x-data="{show: true}" x-init="setTimeout(() => show = false, 2000)" x-show="show"
+    class="flash-message">{{session('message')}}</p>
 @endif
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,8 +10,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link href="/css/app.css" rel="stylesheet">
-    <!--Font Awesome-->
-    <script src="https://kit.fontawesome.com/425e93ca1f.js" crossorigin="anonymous"></script>
+    <!--Alpine JS-->
+    <script src="//unpkg.com/alpinejs" defer></script>
 </head>
 
 <body>
